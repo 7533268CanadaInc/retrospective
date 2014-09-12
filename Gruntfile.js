91c411a032ebaa55db8265798c2733189c04c12c
gregtandiono <gregtandiono@gmail.com>
2014-09-12 13:42:05 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    stylus: {
+      build: {
+        files: {
+          'stylesheets/main.css': 'stylus/app.styl'
+        }
+      }
+    },
+
+    cssmin: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'stylesheets/main.min.css': 'stylesheets/main.css'
+        }
+      }
+    },
+
+    react: {
+      files: {
+        expand: true,
+        cwd: 'javascripts/react/src/',
+        src: ['**/*.js'],
+        dest: 'javascripts/react/build'
+      }
+    },
+
+    uglify: {
+      jsx: {
+        files: {'javascripts/react/react-files-min.js':
+          [
+            'javascripts/react/build/*.js'
+          ]
+        },
+        options: {
+          mangle: false,
+          preserveComments: false
+        }
+      }
+    },
+
+    watch: {
+      stylesheets: {
+        files: ['stylesheets/main.css', 'stylus/**/*.styl'],
+        tasks: ['stylus', 'cssmin']
+      },
+      react: {
+        files: 'javascripts/react/src/**/*.js',
+        tasks: 'react'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-react');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['cssmin', 'stylus', 'react', 'uglify']);
+  grunt.registerTask('production', ['uglify']);
+};
